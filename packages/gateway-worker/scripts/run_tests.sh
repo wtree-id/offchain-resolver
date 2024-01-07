0906ca8f1b0cc9210ce6502160546f5c094318e6
Mikhail <16622558+mmv08@users.noreply.github.com>
2024-01-07 18:14:03 +0100

    update test script

--- packages/gateway-worker/scripts/run_tests.sh
+++ packages/gateway-worker/scripts/run_tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-npx wrangler dev --var USE_TEST_DB:true OG_PRIVATE_KEY:0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 & wrangler_pid=$!
-ganache > /dev/null & ganache_pid=$!
-sleep 10
+npx wrangler dev --var USE_TEST_DB:true OG_PRIVATE_KEY:0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 & 
+ganache > /dev/null & 
+sleep 5
 npm run test:run
 
 # the node and the local dev server end up running in different processes, i didn't find a way to kill them
