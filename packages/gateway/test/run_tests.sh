0f7ee1e4d7d913f95b1793f42d411af7ee08e9f4
Mikhail <16622558+mmv08@users.noreply.github.com>
2024-01-07 18:08:21 +0100

    test

--- packages/gateway/test/run_tests.sh
+++ packages/gateway/test/run_tests.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-npx hardhat node > /dev/null & hardhat_pid=$!
+npx hardhat node > /dev/null &
 sleep 5
 npm run test:run
-
-if [ ${hardhat_pid:-0} -gt 1 ]; then
-    echo "Killing hardhat with PID ${hardhat_pid}"
-    kill $hardhat_pid
-fi
+pkill -f hardhat
